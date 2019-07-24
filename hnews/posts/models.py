83b1de5eb822fbac07dc236ce157631b9a67f360
James Petley <James@student-10-245-174-19.wlan.dur.ac.uk>
2019-07-24 15:32:57 +0100

    Added post model.

---
+++ hnews/posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    creator = models.ForeignKey(User, related_name='posts',
+    on_delete = models.SET_NULL, null = True,)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    url = models.URLField()
